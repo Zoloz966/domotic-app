b987704f36d4bdc6062f4171baf6163fb5a3e542
Zolouz966 <zrodrigo96@gmail.com>
2025-10-27 20:21:43 -0400

    we end all first layout

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,12 +1,11 @@
 import { Component, type OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './layout.html',
 })
 export default class Layout implements OnInit {
-
-  ngOnInit(): void { }
-
+  ngOnInit(): void {}
 }
